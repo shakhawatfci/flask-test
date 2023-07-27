23e5a148ac8b0be76a061c41a28460d4f9e53865
shakhawatfci <94shakha.18@gmail.com>
2023-07-27 23:35:29 +0600

    validation insert

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask , render_template , redirect , url_for , request
-
+from flask import Flask, render_template, request, redirect, url_for, flash
 import datetime
 
 from flask_sqlalchemy import SQLAlchemy
@@ -21,7 +20,9 @@ def inject_current_year():
 @app.route('/')
 def index():
     all_records = User.query.all()
-    message = request.args.get('message')
+    message = ''
+    if(request.args.get('message')):
+        message = request.args.get('message')
     return render_template('index.html',users = all_records , message = message)
 
 @app.route('/about')
@@ -29,20 +30,57 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/todo/<id>')
-def showTodo(id):
-    return id
+@app.route('/user/create')
+def createUser():
+    return render_template('create_user.html',errors=None)
 
 
-@app.route('/todo/delete/<id>')
+@app.route('/user',methods=['POST'])
+def saveUser():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    errors = validate_input(name, password , email)
+
+    if errors:
+        # If there are validation errors, render the form again with the error messages
+        return render_template('create_user.html', errors=errors)
+
+    password_hash = generate_password_hash(password)
+    user = User(name=name, email=email , password=password_hash)
+    db.session.add(user)
+    db.session.commit()
+    
+    return redirect('/')
+
+
+@app.route('/todo/user/<id>')
 def deleteTodo(id):
     user_to_delete = User.query.filter_by(id=id).first()
     if user_to_delete:
      db.session.delete(user_to_delete)
      db.session.commit()
-     return redirect(url_for('/', message = "User successfully deleted"))
+     return redirect('/')
+    #  return redirect(url_for('/', message = "User successfully deleted"))
 
+def validate_input(name, password , email):
+    errors = []
 
+    # Validate name
+    if not name:
+        errors.append('Username is required.')
+        
+    if not email:
+        errors.append('email is required.')
+
+    # Validate password
+    if not password:
+        errors.append('Password is required.')
+    elif len(password) < 6:
+        errors.append('Password must be at least 6 characters long.')
+
+    return errors
 
 if __name__ == '__main__':
     app.run(debug=True)
